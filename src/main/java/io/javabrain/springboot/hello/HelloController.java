79e3974fc945d54c515652e69474b6a3d96f631f
siyong.zhu <siyong.zhu@irdeto.com>
2019-03-26 15:03:45 -0400

    change hello root path

--- src/main/java/io/javabrain/springboot/hello/HelloController.java
+++ src/main/java/io/javabrain/springboot/hello/HelloController.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	public String sayHi() {
 		return "Hi";
 	}
